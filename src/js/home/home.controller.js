7c1e239c6b4efd748aa2560c6bad2b4c9c12ad90
fourgates <ninan.phillip@gmail.com>
2017-03-11 19:50:57 -0500

    start of list and sorting

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -1,12 +1,24 @@
 class HomeCtrl {
-  constructor(AppConstants) {
+  constructor(AppConstants, $scope) {
     'ngInject';
-    
+    this.list = [{
+    	aile:5,
+    	item: "Milk"
+    }]
     this.appName = AppConstants.appName;
 
   }
 
+  getList(){
+    return this.list;
+  }
 
+  addItem(){
+  	this.list.push({
+    	aile:5,
+    	item: "Milk"
+    })
+  }
 }
 
 export default HomeCtrl;
