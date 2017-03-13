88daa6601af1211f55bcf312305b0fae932300b6
fourgates <ninan.phillip@gmail.com>
2017-03-12 21:30:50 -0400

    create TODO for functionality and init selectize. for some reason the styles are off

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -1,3 +1,18 @@
+import selectize from 'selectize';
+
+// TODO - SmartGroceryList Features 
+// 1. add checkbox to each row and strike through if checked
+// 2. if checkbox is marked display a delete button
+// 3. each row should have an autocomplete that gets populated from previous lists
+// 4. be able to create a recipe and save it. when users want to cook that again
+//    they can add it and all the ingrediants get added to list
+// 5. integrate amazon alexa shopping list 
+// 6. be able to select a grocery store and based on the name of an item see if anything is on sale
+//    or if there are any coupes.
+//    ie. if i put milk on the list and im going to shop rite, search shop rite coupons to see if 
+//        any milk is on sale 
+// 7. build out hardware to be able to clip phone to a shopping cart for quick use 
+// 8. 
 class HomeCtrl {
   constructor(AppConstants, $scope) {
     'ngInject';
@@ -7,6 +22,10 @@ class HomeCtrl {
     }]
     this.appName = AppConstants.appName;
 
+    console.log('test', $('select'));
+    $('select').selectize({
+      options:[]
+    });
   }
 
   getList(){
