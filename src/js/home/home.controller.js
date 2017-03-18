7ba91178044a3dc326eec159896ac38ce9d05e81
fourgates <ninan.phillip@gmail.com>
2017-03-18 12:54:38 -0400

    added a date picker to list component

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -1,28 +1,58 @@
 import selectize from 'selectize';
+import bootstrap from 'angular-ui-bootstrap';
 
 // TODO - SmartGroceryList Features 
-// 1. add checkbox to each row and strike through if checked
-// 2. if checkbox is marked display a delete button
+//  -1-. add checkbox to each row and strike through if checked
+//  -2-. if checkbox is marked display a delete button
 // 3. each row should have an autocomplete that gets populated from previous lists
 // 4. be able to create a recipe and save it. when users want to cook that again
 //    they can add it and all the ingrediants get added to list
 // 5. integrate amazon alexa shopping list 
 // 6. be able to select a grocery store and based on the name of an item see if anything is on sale
-//    or if there are any coupes.
+//    or if there are any coupons.
 //    ie. if i put milk on the list and im going to shop rite, search shop rite coupons to see if 
 //        any milk is on sale 
 // 7. build out hardware to be able to clip phone to a shopping cart for quick use 
-// 8. 
+// 8. add a date picker to keep track of shopping list dat
+// 9. need to create a data structure to store weekly lists (needed to populate autocomplete from previous week(s))
+//     date
+//     list of items
 class HomeCtrl {
 
-  constructor(AppConstants, $scope, $timeout) {
+  constructor(AppConstants, $scope, $timeout, uibDateParser) {
     'ngInject';
-    this.list = []
+
+    // app
     this.appName = AppConstants.appName;
     this._$timeout = $timeout;
+
+    // date picker
+    this.date = new Date();
+    this.isOpen = false;
+
+    this.dateOptions = {
+      dateDisabled: this.disabled,
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+
     // watch the shopping list
+    this.list = []
     $scope.$watchCollection(() => this.list, this.onListChange());
   }
+
+  // date picker fnc's
+  disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+  openDatePicker(){
+    this.isOpen = true;
+  };
 
   // never let the list be empty 
   onListChange (newValue, oldValue) {
