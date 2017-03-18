406759d6ef53639dc736dd88360041508bd3b762
fourgates <ninan.phillip@gmail.com>
2017-03-18 14:23:12 -0400

    abstracted load list to its own function (will further abstract this to its own service later)

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -5,6 +5,44 @@ class HomeCtrl {
 
     // app
     this.appName = AppConstants.appName;
+  	this.loadResults();
+  }
+
+  loadResults(){
+  	this.results = [
+	    {
+	  		date: new Date(),
+	  		numberOfItem: 1,
+	  		list: [
+	  			{
+	              item: "Butter",
+	              aile: 3
+	            }
+	  		]
+
+	  	},
+	  	{
+	  		date: new Date(),
+	  		numberOfItem: 2,
+	  		list: [
+	  			{
+	              item: "Milk",
+	              aile: 1
+	            },
+	            {
+	              item: "Butter",
+	              aile: 3
+	            }
+	  		]
+	  	}
+  	];
+  }
+  getList(){
+  	return this.results;
+  }
+
+  loadList(list){
+  	this.currentList = list;
   }
 }
 
